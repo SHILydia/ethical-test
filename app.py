e1aa9fa0d086db0c807c92e91fc2a8aab1698474
SHILydia <142367227+SHILydia@users.noreply.github.com>
2024-03-15 20:38:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,11 @@
 
 # In[ ]:
 
-
 from flask import Flask,request,render_template
+import datetime
+import sqlite3
+from flask import Markup
+
 app = Flask(__name__)
 
 name_flag=0
@@ -20,11 +23,31 @@ def main():
     if name_flag==0:
         name=request.form.get("name")
         name_flag=1
+        conn = sqlite3.connect('log.db')
+        c=conn.cursor()
+        timestamp=datetime.datetime.now()
+        c.execute("insert into employee (name, timestamp) values(?,?)",(name,timestamp))
+        conn.commit()
+        c.close
+        conn.close
     return(render_template("main.html",name=name))
 
 @app.route("/ethical_test",methods=["GET","POST"])
 def ethical_test():
     return(render_template("ethical_test.html"))
+
+@app.route("/query",methods=["GET","POST"])
+def query():
+    conn = sqlite3.connect('log.db')
+    c = conn.execute('select * From employee')
+    r=""
+    for row in c:
+        r=r+str(row)+"<br>"
+    print(r)
+    r=Markup(r)
+    c.close
+    conn.close
+    return(render_template("query.html",r=r))
 
 @app.route("/answer",methods=["GET","POST"])
 def answer():
