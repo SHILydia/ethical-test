4350d4342b35d7d456beb80fe780cb6358bcad7a
SHILydia <142367227+SHILydia@users.noreply.github.com>
2024-03-15 19:33:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,20 @@
 
 from flask import Flask,request,render_template
 app = Flask(__name__)
+
+name_flag=0
+name=""
+
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
 
 @app.route("/main",methods=["GET","POST"])
 def main():
-    name=request.form.get("name")
+    global name_flag,name
+    if name_flag==0:
+        name=request.form.get("name")
+        name_flag=1
     return(render_template("main.html",name=name))
 
 @app.route("/ethical_test",methods=["GET","POST"])
@@ -34,7 +41,6 @@ def end():
 
 if __name__ == "__main__":
     app.run()
-
 
 # In[ ]:
 
